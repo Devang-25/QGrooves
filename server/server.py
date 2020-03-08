1c67b0f0abf987d683db866b40fe008402276e87
Ivan Carvalho <ivan.carvalho@alumni.ubc.ca>
2020-03-08 03:17:47 -0700

    Add boiler-plate Flask code

---
+++ server/server.py
@@ -0,0 +1,19 @@
+#!flask/bin/python
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET"])
+def index():
+    """Receives a request with a quantum circuit, generates music,
+    and saves the .wav file to MongoDB
+    """
+    user_id = request.args.get("userId")
+    circuit_json = request.args.get("circuitJson")
+
+    return {"test": 0}
+
+
+if __name__ == "__main__":
+    app.run(port=3000)
