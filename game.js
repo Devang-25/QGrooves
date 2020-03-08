b0bbb5a760c2d136b799d2094a34baf0cb18611c
sroarty <43792819+sroarty@users.noreply.github.com>
2020-03-08 04:25:21 -0700

    Add files via upload

--- game.js
+++ game.js
@@ -15,7 +15,7 @@ var hozLineColor = 0x7546eb;        // Horizontal line color
 var dotSize = 15;                   // Radius of dots
 var dotColorDefault = 0xeb4672;     // Color of inactive dots
 var dotColorActive = 0x000099;      // Color of active dots
-//---CONSTANTS -- DON'T TOUCH THIS SHIT
+//---CONSTANTS -- DON'T TOUCH THIS 
 const Y_OFFSET = 200;               // y-offset for the grid
 const LEFTY = 325;                  // Fudge-factor
 const X_OFFSET = 130;               // Left-most X value for grid
@@ -23,6 +23,8 @@ const LINESPACE = 40;               // Space between grid-lines
 const ROWS = 6;                     // Number of rows in grid
 const COLUMNS = 20;                 // Number of columns in grid
 const TRIG_RADIUS = LINESPACE / 3;  // Radius at which a block is 'activated' (for placing blocks)
+const BLOCK_SELECT_Y = 150;
+const BLOCK_SELECT_x = 450;
 //------------
 var vertLines = [];                 // Array of vertical lines
 var hozLines = [];                  // Array of horizontal lines
@@ -30,16 +32,63 @@ var title;                          // Title text
 var mouseTrack;                     // DEBUG - Tracks mouse x,y on-screen
 var testbock;                       // DEBUG - test block container
 var game = new Phaser.Game(config);
-var grid = new Array(ROWS);   // STORES IMPORTANT INFORMATION ABOUT GRID! HOLDS INFO FOR JSON
+var grid = new Array(ROWS);         // STORES IMPORTANT INFORMATION ABOUT GRID! HOLDS INFO FOR JSON
 for(let i = 0; i < grid.length; i++) {
     grid[i] = new Array(COLUMNS);
 }
-function preload() {
-    this.load.image('BACKGROUND', 'assets/background.png'); // preload background image
-    this.load.image('TESTBLOCK', 'assets/testblock.png'); // DEBUG-- preoad test block image
+const BLOCK_NAMES = ["CH","CRZ","H","ID","RX",
+                     "RY","S","S_UP","T","T_UP",
+                     "U1","U2","U3","X","Y"];
+
+// -- Elements which are draggable by the user
+class DragBlock {
+    constructor(img, curScene) {
+            this.attachedTo = null;
+            this.img = img;
+            this.img.setInteractive();
+            curScene.input.setDraggable(this.img);
+            this.img.on('dragend', snapToGrid.bind(this));
+            this.id;
+    }
 }
 
+// -- COMPOSES THE GRID TO BE SENT TO BACKEND
+class Block {
+    constructor(x, y, curScene) {
+        this.blockType = "";        // STRING: holds the block-type
+        this.entangID = 0;          // NUM: contains ID if block is entangled with other blocks
+        this.locX = x;              // NUM: relative x-loc of block -- NOT SENT TO BACKEND
+        this.locY = y;              // NUM: relative y-loc of block -- NOT SENT TO BACKEND
+        this.focus = false;         // BOOL: if mouse is over: in focus
+        this.active = false;        // BOOL: the block is active (exists)
+            
+        // ELIPSE-SHAPE: dot drawn at [x,y] loc -- NOT SENT TO BACKEND
+        this.dot = curScene.add.ellipse(this.locX, this.locY, dotSize, dotSize, dotColorDefault);
+        this.dot.setInteractive();
+        this.dot.on('pointerover', () => { this.dot.fillColor = dotColorActive; this.focus = true;});
+        this.dot.on('pointerout', () => { this.dot.fillColor = dotColorDefault; this.focus = false;});
+    }
+}
 
+// PHASER METHOD -- preloads assets for the create() function
+function preload() {
+    this.load.image('BACKGROUND','assets/background.png'); 
+    this.load.image('CH', 'assets/icon_cH.png'); 
+    this.load.image('CRZ', 'assets/icon_crz.png'); 
+    this.load.image('H', 'assets/icon_H.png'); 
+    this.load.image('ID', 'assets/icon_ID.png'); 
+    this.load.image('RX', 'assets/icon_RX.png');
+    this.load.image('RY', 'assets/icon_RY.png');
+    this.load.image('S', 'assets/icon_S.png');
+    this.load.image('S_UP', 'assets/icon_S_up.png'); 
+    this.load.image('T', 'assets/icon_T.png'); 
+    this.load.image('T_UP', 'assets/icon_T_Up.png'); 
+    this.load.image('U1', 'assets/icon_U1.png'); 
+    this.load.image('U2', 'assets/icon_U2.png'); 
+    this.load.image('U3', 'assets/icon_U3.png'); 
+    this.load.image('X', 'assets/icon_X.png');
+    this.load.image('Y', 'assets/icon_Z.png'); 
+}
 
 function create() {
     // Enables blocks to be dragged -- DON'T TOUCH IT
@@ -47,36 +96,23 @@ function create() {
         gameObject.x = dragX;
         gameObject.y = dragY;
     });
+
     // adds background image
     this.add.image(512, 384, 'BACKGROUND'); 
-    
-    //--TEST CODE   
-    testblock = this.add.image(100,200,'TESTBLOCK');
-    testblock.setInteractive();
-    this.input.setDraggable(testblock);
-    testblock.on('dragend', snapToGrid);
-    //--TEST CODE
+
+    // all of the possible blocks a user can drag
+    var blockList = [];
+    for(let i = 0; i < BLOCK_NAMES.length; i++) {
+        blockList.push(new DragBlock
+            (this.add.image(BLOCK_SELECT_Y + i*50,BLOCK_SELECT_x,BLOCK_NAMES[i]),this));
+    }
 
     // title text
     title = this.add.text(400, 100, "Q-Groov :-]", { fontFamily: '"Segoe UI"', fontSize: 50 });
+    
+    
     // DEBUG: tracks mouse on-screen
     mouseTrack = this.add.text(150, 100, "", { fontFamily: '"Segoe UI"', fontSize: 50 });
-
-    // -- COMPOSES THE GRID TO BE SENT TO BACKEND
-    class Block {
-        constructor(x, y, curScene) {
-            this.blockType = ""; // STRING: holds the block-type
-            this.entangID = 0;   // NUM: contains ID if block is entangled with other blocks
-            this.locX = x; 
-            this.locY = y;              // NUM: column which contains the block -- NOT SENT TO BACKEND
-            this.active = false;        // BOOL: if mouse is over: active
-            // ELIPSE-SHAPE: dot drawn at [x,y] loc -- NOT SENT TO BACKEND
-            this.dot = curScene.add.ellipse(this.locX, this.locY, dotSize, dotSize, dotColorDefault);
-            this.dot.setInteractive();
-            this.dot.on('pointerover', () => { this.dot.fillColor = dotColorActive; this.active = true;});
-            this.dot.on('pointerout', () => { this.dot.fillColor = dotColorDefault; this.active = false;});
-        }
-    }
 
     // Draw the horizontal lines of the grid - LEAVE IT ALONE
     for (let y = 0; y < ROWS; y++) {
@@ -97,19 +133,26 @@ function create() {
             grid[x][y] = new Block(xval, yval, this);
         }    
     }
-    console.log(grid);
 }
 
 // snaps the active block to the grid
 function snapToGrid() {
-    console.log(grid);
+
+    // if DragBlock is attached to a block, de-activate that block and detatch
+    if(this.attachTo != null) {
+        this.attachTo.active = false;
+        this.attachTo = null;
+    }
+
     for (let a = 0; a < ROWS; a++) {
         for (let b = 0; b < COLUMNS; b++) {
-            if(grid[a][b].active) {
-                this.x = grid[a][b].locX;
-                this.y = grid[a][b].locY;
-                return;  
-            }
+            if(grid[a][b].focus) {
+                this.img.x = grid[a][b].locX;
+                this.img.y = grid[a][b].locY;
+                this.attachTo = grid[a][b];
+                grid[a][b].active = true;
+                return;
+           }
         }
     }
 }
@@ -117,24 +160,7 @@ function snapToGrid() {
 function update() {
     //SHOWS MOUSE COORDS IN SCREEEN FOR DEBUGGING
     mouseTrack.text = `[${this.input.x} , ${this.input.y}]`;
-
-    // SAVING THIS FOR A RAINY DAY BUT IT'S DUMB
-    /* for (let x = 0; x < COLUMNS; x++) {
-        for (let y = 0; y < ROWS; y++) {
-            // proximity between mouse and current block
-            let deltaX = (Math.abs(grid[x, y].x - this.input.x));
-            let deltaY = (Math.abs(grid[x, y].y - this.input.y));
-
-            // if mouse cursor is within activation radius, change the color of block's dot
-            if (deltaX < TRIG_RADIUS && deltaY < TRIG_RADIUS) {
-                grid[x, y].dot.fillColor = dotColorActive;
-            }
-            else if (grid[x, y].dot.fillColor != dotColorDefault) {
-                grid[x, y].dot.fillColor = dotColorDefault;
-            }
-        }
-    }*/
 }
 
 function render() {
-}
+} 
